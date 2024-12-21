f6ae3f7a20c0e23702202f9a347167147ebd03c3
nicolas <zeross30@gmail.com>
2024-12-21 11:33:19 +0100

    Enhance WordPress configuration in docker-entrypoint.sh
    - Added environment variable display for database configuration to aid in debugging.
    - Implemented retrieval and insertion of WordPress security keys into wp-config.php for improved security.
    - Updated wp-config.php setup to ensure proper configuration of database and table prefix using environment variables.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,6 +21,13 @@ setup_wp_config() {
         cp /var/www/vhosts/localhost/html/wp-config-sample.php /var/www/vhosts/localhost/html/wp-config.php
     fi
 
+    # Afficher les variables d'environnement
+    echo "WORDPRESS_DB_NAME: ${WORDPRESS_DB_NAME}"
+    echo "WORDPRESS_DB_USER: ${WORDPRESS_DB_USER}"
+    echo "WORDPRESS_DB_PASSWORD: ${WORDPRESS_DB_PASSWORD}"
+    echo "WORDPRESS_DB_HOST: ${WORDPRESS_DB_HOST}"
+    echo "WORDPRESS_TABLE_PREFIX: ${WORDPRESS_TABLE_PREFIX}"
+
     # Configuration de la base de données
     sed -i "s/define( 'DB_NAME', '.*' );/define( 'DB_NAME', '${WORDPRESS_DB_NAME}' );/" /var/www/vhosts/localhost/html/wp-config.php
     sed -i "s/define( 'DB_USER', '.*' );/define( 'DB_USER', '${WORDPRESS_DB_USER}' );/" /var/www/vhosts/localhost/html/wp-config.php
@@ -29,6 +36,11 @@ setup_wp_config() {
     
     # Configuration du préfixe des tables
     sed -i "s/\$table_prefix = 'wp_';/\$table_prefix = '${WORDPRESS_TABLE_PREFIX}';/" /var/www/vhosts/localhost/html/wp-config.php
+
+    # Récupération des clés de sécurité
+    curl -O https://api.wordpress.org/secret-key/1.1/salt
+    sed -i '/#@-/r salt' wp-config.php
+    sed -i '/#@+/,/#@-/d' wp-config.php
 }
 
 # Installation de WordPress
